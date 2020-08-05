7eb04fd3a090385b6f6a285569c2e4eb1e390651
Duarte Aragao <daragao@gmail.com>
2020-08-05 22:29:44 +0100

    uncommented upload function script

--- lambda/uploadFunctions.sh
+++ lambda/uploadFunctions.sh
@@ -4,4 +4,4 @@ set -e
 cd $1
 zip ../$1.zip *.*
 cd ..
-#aws lambda update-function-code --function-name  $1 --zip-file fileb://$1.zip
+aws lambda update-function-code --function-name  $1 --zip-file fileb://$1.zip
